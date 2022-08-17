330745100623eda93d99076a1a31ffe233f2b747
flyingjjang051 <flyjjang051@gmail.com>
2022-08-17 13:55:47 +0900

    naver id 설정

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
 const express = require("express");
 const morgan = require("morgan");
 const axios = require("axios");
+const cors = require("cors");
 const app = express();
-
+const dotenv = require("dotenv").config();
+const NAVER_ID = process.env.NAVER_ID;
+const NAVER_SECRET_ID = process.env.NAVER_SECRET_ID;
 app.set("port", process.env.PORT || 8099);
 const port = app.get("port");
 app.use(morgan("dev"));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("hello express");
 });
-app.get("/papago", (req, res) => {
+app.post("/papago", (req, res) => {
+  const txt = req.body.txt;
+  const language = req.body.language;
   axios({
     url: "https://openapi.naver.com/v1/papago/n2mt",
     method: "POST",
     params: {
       source: "ko",
-      target: "en",
-      text: "김정은 북한 국무위원장이 코로나19 위기가 완전히 해소됐다고 선언한 가운데, 김정은의 동생인 김여정 노동당 부부장은 코로나19 바이러스가 남측으로부터 북한에 유입됐다고 주장했다. 그러면서 김여정은 “우리는 반드시 강력한 대응을 해야 한다”고 했다",
+      target: language,
+      text: txt,
     },
     headers: {
-      "X-Naver-Client-Id": "5vUtAmLfmqV7l_2Q4E2p",
-      "X-Naver-Client-Secret": "Oq5AQ8LPzt",
+      "X-Naver-Client-Id": NAVER_ID,
+      "X-Naver-Client-Secret": NAVER_SECRET_ID,
       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     },
-  }).then((response) => {
-    console.log(response.data.message.result.translatedText);
-  });
+  })
+    .then((response) => {
+      //console.log(response.data.message.result.translatedText);
+      res.json({ result: response.data.message.result.translatedText });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send(error);
+    });
 });
 app.listen(port, () => {
   console.log(`${port}번에서 서버 대기중`);
